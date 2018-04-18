486994a4e5ef967aa8f4c0c8fd8a13ae92b9e0fe
tahmed <tahmed@netprospex.com>
2018-04-18 13:05:52 -0400

    Committing full app to repo

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # dailyfeedingest
 Web app to ingest daily feeds 
+
+Author - Tarif Ahmed
+Date - 4.18.2018
+
+
+1) Design
+
+The application was designed to run locally on a laptop using SQLite and Flask as the underlying tools. The app currently launches a local webpage which allows the user to upload a file via upload form. The file is then parsed in Python using CSV reader, converted into a list, and each list entry is then inserted in the database
+
+
+2) Implementation
+
+Databse Initialization 
+
+-Clone the app to your laptop from Git
+-Navigate to the directory the app is located in and open a Shell command line 
+-Execute the following code to initialize the database:
+
+		python
+		from main import db
+		db.create_all() 
+
+To validate whether the database was created successfully, please navigate back to the app directory and confirm the existance of the app.sqlite file.
+
+Now launch the app:
+	python main.py
+
+The web interface can be accessed:
+	http://localhost:5000/ 
+
+From there, upload the file via the URL that labelled "Upload daily feed"
+
+To check whether the app has correctly inserted the data into the database, you can load the app.sqlite file into a sqlite reader/viewer (free tools available at http://sqliteviewer.flowsoft7.com/)
+
+
+3) Next steps
+
---
+++ main.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, redirect, request
+from werkzeug import secure_filename
+from flask_sqlalchemy import SQLAlchemy
+import os
+import csv
+from collections import namedtuple
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
+db = SQLAlchemy(app) 
+
+class Note(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(80))
+	body = db.Column(db.Text)
+
+class fileEntry(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	customer_id = db.Column(db.Integer)
+	first_name = db.Column(db.String(100))
+	last_name = db.Column(db.String(100))
+	street_address = db.Column(db.String(200))
+	state = db.Column(db.String(30))
+	zip_code = db.Column(db.Integer)
+	requested_change = db.Column(db.String(10))
+	product_id = db.Column(db.Integer)
+	product_name = db.Column(db.String(100))
+	purchase_price = db.Column(db.Integer)
+	transaction_time = db.Column(db.String(100))
+
+@app.route("/", methods=["GET", "POST"])
+def create_note():
+    if request.method == "POST":
+
+    	title = request.form["title"]
+        body = request.form["body"]
+
+        note = Note(title=title, body=body)
+
+        db.session.add(note)
+        db.session.commit()
+
+        return redirect("/")
+    else:
+        return render_template("create_note.html")
+
+@app.route('/test')
+def test_page():
+	return render_template('test.html')
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploading_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save(secure_filename(f.filename))
+		print 'file uploaded successfully'
+
+		with open(f.filename,'rb') as file:
+			reader = csv.reader(file, delimiter='\t')
+			for row in reader:
+				row_as_list = list(row)
+				customer_id = row_as_list[0]
+				first_name = row_as_list[1]
+				last_name = row_as_list[2]
+				street_address = row_as_list[3]
+				state = row_as_list[4]
+				zip_code = row_as_list[5]
+				requested_change = row_as_list[6]
+				product_id = row_as_list[7]
+				product_name = row_as_list[8]
+				purchase_price = row_as_list[9]
+				transaction_time = row_as_list[10]
+
+				file_entry = fileEntry(customer_id = customer_id, first_name = first_name, last_name = last_name, street_address = street_address, state = state, zip_code = zip_code, requested_change = requested_change, product_id = product_id, product_name = product_name, purchase_price = purchase_price, transaction_time = transaction_time)
+
+				db.session.add(file_entry)
+				db.session.commit()
+
+		return redirect("/")
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
